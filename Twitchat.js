b355f966b65e438c69e6a391a50a1850ff91ae9a
Edrig <vivarais51@gmail.com>
2022-10-28 16:24:50 +0200

    Fix Readme & module.json

--- Twitchat.js
+++ Twitchat.js
@@ -16,7 +16,9 @@ function update()
 function moduleParameterChanged(param)
 {
 	if(param.name == "giveLinkForOAuth") {
-		local.parameters.linkForOAuth.set("https://id.twitch.tv/oauth2/authorize?response_type=token&client_id="+local.parameters.clientID.get()+"&&redirect_uri=http://localhost:3000&scope=chat%3Aread+chat%3Aedit+channel%3Amoderate+whispers%3Aread+whispers%3Aedit");
+		local.parameters.linkForOAuth.set("https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=" 
+			+ local.parameters.clientID.get()
+			+"&redirect_uri=http://localhost:3000&scope=chat%3Aread+chat%3Aedit+channel%3Amoderate+whispers%3Aread+whispers%3Aedit");
 	}
 	if(param.name == "connected") {
 		local.parameters.identified.set(false);
@@ -56,22 +58,23 @@ function wsDataReceived(data)
 	--------------------------------------------------------------------------------------------------------------*/
 function Privmsg(message) {
 	local.send("PRIVMSG #"+local.parameters.channelName.get().toLowerCase()+" :"+message);
-};
+}
 
 function Commande(data) {
 	
-/*	
-	var baseURL = "https://id.twitch.tv/oauth2/authorize?response_type=token";
-    var clientId = "client_id=" + local.parameters.client_ID.get();
-    var redirectUri = "redirect_uri=" + local.parameters.redirect_URL.get();
+var baseURL = "https://id.twitch.tv/oauth2/authorize?response_type=token";
+	var client_ID = local.parameters.clientID.get();
+	script.log(client_ID);
+    var clientId = "client_id=" + client_ID;
+    var redirectUri = "redirect_uri=" + "http://localhost:3000";  //local.parameters.redirect_URL;
     var scope = "scope=chat:read chat:edit channel:moderate whispers:read whispers:edit";
 	var client_secret = "ts7ahow6wptddqsr4qj4vgyjalhbmi";
     var state = "state=c3ab8aa609ea11e793ae92361f002671";
-	script.log(baseURL + "&" + clientId + "&" + redirectUri + "&" + scope + "&" + state);
-	local.sendGET("https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=9az83rn2boolkhfais3gy65bw58xp0&redirect_uri=http://localhost:3000&scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls&state=c3ab8aa609ea11e793ae92361f002671");
+	script.log(baseURL + "&" + clientId + "&" + redirectUri + "&" + scope);
+	//local.send(baseURL + "&" + clientId + "&" + redirectUri + "&" + scope);
+	//local.sendGET("https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=9az83rn2boolkhfais3gy65bw58xp0&redirect_uri=http://localhost:3000&scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls&state=c3ab8aa609ea11e793ae92361f002671");
     //this.webview.Source = new System.Uri(baseURL + "&" + clientId + "&" + redirectUri + "&" + scope + "&" + state, System.UriKind.Absolute);
 	//local.send(baseURL + "&" + clientId + "&" + redirectUri + "&" + scope + "&" + state);
 	
-*/
-};
+}
 
